57287c6153c633cb80c04e819d029237faa068fc
FRANCISCO WILLIAM DE OLIVEIRA LIMA <francisco.wolima@senacsp.edu.br>
2022-03-24 21:49:40 -0300

    meu componete com props

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header stitle = " Meu primeiro componente React" />
+      <Header title = " Um componente com props" />
+      <h1>São Paulo</h1>
+      <h2>Senac Campinas</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+function Header(props) {
+    return (
+        <header>
+         <h1>{props.title}</h1>
+        </header>
+        
+        )
+        
+    
+}
+
+export default Header;
